1d62de46de5a7bf46acb4c1800c659e16456e7b4
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-08-20 11:56:48 -0400

    Parse and validate data from yaml file

--- build.py
+++ build.py
@@ -1,8 +1,36 @@
 """Inject poem data into template to create epub source."""
 
 
+from ruyaml import YAML
+
+
+def get_data():
+    """Get data from poem.yaml and validate it."""
+    yaml = YAML(typ="safe")
+
+    with open("poem.yaml") as fin:
+        data = yaml.load(fin)
+
+    # Validate data
+    assert "title" in data
+    assert "author" in data
+    assert "poem" in data
+
+    assert isinstance(data["poem"], list)
+
+    for section in data["poem"]:
+        assert isinstance(section, list)
+
+        for line in section:
+            assert isinstance(line, str)
+
+    return data
+
+
 def main():
-    print("Hello, Wotld!")
+    """Entrypoint for script."""
+    data = get_data()
+    print(data)
 
 
 if "__main__" in __name__:
