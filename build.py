c2e1fb7edd9d04247f7b31281428b1997a40ed18
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-08-20 14:23:18 -0400

    Add logic for generating toc.ncx

--- build.py
+++ build.py
@@ -6,6 +6,9 @@ from pathlib import Path
 from ruyaml import YAML
 
 
+BUILD_PATH = Path("build/OEBPS")
+
+
 def get_data():
     """Get data from poem.yaml and validate it."""
     yaml = YAML(typ="safe")
@@ -29,22 +32,44 @@ def get_data():
     return data
 
 
+def jinja2_env():
+    """Create jinja2 env that can find templates under template/OEBPS."""
+    loader = FileSystemLoader(searchpath="template/OEBPS")
+    return Environment(loader=loader)
+
+
 def inject_toc_html(title: str):
     """Inject title to create toc.html file."""
-    loader = FileSystemLoader(searchpath="template/OEBPS")
-    env = Environment(loader=loader)
+    env = jinja2_env()
     template = env.get_template("toc.html.template")
 
     content = template.render(title=title)
 
-    Path("build/OEBPS/toc.html").write_text(content)
+    file = BUILD_PATH / "toc.html"
+    file.write_text(content)
+
+
+def inject_toc_ncx(title: str, author: str):
+    """Inject title and author to create tox.ncs file."""
+    env = jinja2_env()
+    template = env.get_template("toc.ncx.template")
+
+    content = template.render(title=title, author=author)
+
+    file = BUILD_PATH / "toc.ncx"
+    file.write_text(content)
 
 
 def main():
     """Entrypoint for script."""
     data = get_data()
 
-    inject_toc_html(title=data["title"])
+    title = data["title"]
+    author = data["author"]
+    poem = data["poem"]
+
+    inject_toc_html(title=title)
+    inject_toc_ncx(title=title, author=author)
 
 
 if "__main__" in __name__:
