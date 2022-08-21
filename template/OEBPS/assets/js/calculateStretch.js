d17c83d9d2d54eaa99c5d2b40f23767aefa45c46
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-08-20 23:37:41 -0400

    Complete calculation of stretch css using javascript

--- template/OEBPS/assets/js/calculateStretch.js
+++ template/OEBPS/assets/js/calculateStretch.js
@@ -5,7 +5,10 @@
 
 function calculateStretch() {
     const sections = document.getElementsByClassName("section");
-    console.log(`Number of sections: ${sections.length}`);
+
+    // Calculate the position of the end of the section (on the left for Urdu)
+    const sectionEnd = sections[0].getBoundingClientRect().left;
+    let output = "";
 
     for (let i = 0; i < sections.length; i++) {
         const section = sections[i];
@@ -13,7 +16,26 @@ function calculateStretch() {
         const lines = section.getElementsByClassName("stretch");
 
         for (let j = 0; j < lines.length; j++) {
-            console.log(`Line id: ${i}.${j}`);
+            // Let us calculate the position of the end of the line
+            const spans = lines[j].getElementsByTagName("span");
+            const last = spans[spans.length - 1];
+            const end = last.getBoundingClientRect().left;
+
+            const lineGap = end - sectionEnd;
+
+            // Divide the line gap per word and convert to em
+            const wordGap = pixelsToEm(spans[0], lineGap / (spans.length - 1));
+
+            output = output.concat(`\ndiv[id="${i}-${j}"].stretch>span.gap { margin-left: ${wordGap}em; }`)
         }
     }
+
+    console.log(output);
+}
+
+
+function pixelsToEm(element, pixels) {
+    // 1 em in pixels is equal to the fontSize of the element so first calculate that
+    const fontSize = parseFloat(getComputedStyle(element).fontSize);
+    return pixels / fontSize;
 }
